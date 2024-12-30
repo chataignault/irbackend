3174fc08ec9e909773cacddaeecc40b9b60766aa
chataignault <leonchat@orange.fr>
2024-12-30 11:40:11 +0000

    format

--- src/irbackend/utils/yf_helpers.py
+++ src/irbackend/utils/yf_helpers.py
@@ -10,12 +10,16 @@ from .processing import process_ticker_hist
 logger = logging.getLogger(__name__)
 
 
-def read_ticker_(ticker: Ticker, start: dt.datetime, end: dt.datetime, save: bool = False):
+def read_ticker_(
+    ticker: Ticker, start: dt.datetime, end: dt.datetime, save: bool = False
+):
     """
     yfinance API proxy for historical data
     Read if file already exists else fetch
     """
-    file_name = "_".join([ticker, start.strftime("%Y%m%d"), end.strftime("%Y%m%d"), ".parquet"])
+    file_name = "_".join(
+        [ticker, start.strftime("%Y%m%d"), end.strftime("%Y%m%d"), ".parquet"]
+    )
     file_path = os.path.join(os.getcwd(), "data", file_name)
     if os.path.exists(file_path):
         logger.info(f"Reading file {file_name}...")
