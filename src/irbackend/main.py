8071300349fd2d790ab0840b9c8dc697be360c23
chataignault <leonchat@orange.fr>
2025-02-25 14:38:01 +0000

    add kaggle bond dataset router

--- src/irbackend/main.py
+++ src/irbackend/main.py
@@ -3,10 +3,11 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from ..utils import *
 from .routers.yf_router import yf_router
-
+from .routers.ka_router import ka_router
 
 app = FastAPI(title="IR app backend")
 app.include_router(yf_router)
+app.include_router(ka_router)
 
 origins = [
     "http://localhost:3000/react-tutorial",
