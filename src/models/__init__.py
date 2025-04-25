5d92c12738f972781b7972794778fae1cad6be2f
chataignault <leonchat@orange.fr>
2025-04-25 12:13:01 +0100

    add first models example helpers

---
+++ src/models/__init__.py
@@ -0,0 +1,3 @@
+
+from .linear import model_linear_regression
+from .tree import model_decision_tree_regressor, model_random_forest, model_light_gbm
---
+++ src/utils/dataset/supervised.py
@@ -0,0 +1,21 @@
+import polars as pl
+import numpy as np
+from typing import Tuple
+
+
+def get_multidim_ts_scaled_lookback_flat(df:pl.LazyFrame, target_name:str, scaling_factor:float=1., lookback:int = 3)->Tuple[np.ndarray, np.ndarray]:
+    diffs = df.with_columns(pl.exclude("time").diff()).fill_nan(0.).fill_null(0.).drop_nans().drop_nulls()
+    diffs_np = diffs.collect().to_pandas().set_index("time").values
+    
+    X = np.array(
+        [
+            diffs_np[i:i+lookback].flatten()
+            for i in range((len(diffs_np) - lookback))
+        ]
+    ) * scaling_factor
+    Y = diffs.select(pl.col(target_name)).collect().to_pandas().values[lookback:]
+
+    Y = scaling_factor * (Y - np.mean(Y))
+    Y = Y.T[0]
+
+    return X, Y
