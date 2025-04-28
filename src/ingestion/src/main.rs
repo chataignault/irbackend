606f351297331e36ff84e4e7bdf8db52565dfeea
chataignault <leonchat@orange.fr>
2025-04-28 09:14:43 +0100

    add first processing and test

--- src/ingestion/src/main.rs
+++ src/ingestion/src/main.rs
@@ -82,16 +82,33 @@ fn filter_jumps(df: DataFrame, cutoff: f32) -> Result<DataFrame, PolarsError> {
     // logic to filter jumps
     df.lazy()
         .select([all().exclude(["time"]).backward_fill(None)])
-        .with_columns([all().exclude(["time"]).first().alias("__first_row")])
         .with_columns([all()
-            .exclude(["time", "__first_row"])
-            .diff(1, NullBehavior::Drop)
+            .exclude(["time"])
+            .diff(1, NullBehavior::Ignore)
+            .fill_null(0.)
             .clip((-cutoff).into(), cutoff.into())
             .replace(-cutoff, 0.)
             .replace(cutoff, 0.)
-            .cum_sum(false)
-            + col("__first_row")])
-        .drop(["__first_row"])
+            .cum_sum(false)])
+        .collect()
+}
+
+// Some dataset for TS prediction
+fn multidim_ts_scaled_lookback_flat(
+    df: DataFrame,
+    target_name: &str,
+    scaling_factor: f32,
+    lookback: i32,
+) -> Result<DataFrame, PolarsError> {
+    df.lazy()
+        .with_columns([col(target_name).shift(lit(1)).drop_nulls().alias("Y")])
+        .with_columns([all()
+            .exclude(["time"])
+            .diff(1, NullBehavior::Drop)
+            .fill_nan(0.)
+            .fill_null(0.)
+            .drop_nans()
+            .drop_nulls()])
         .collect()
 }
 
@@ -108,4 +125,30 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("{}", polars_df.head(Some(5)));
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_filter_jumps() {
+        let cutoff: f32 = 3.;
+
+        let t = Column::new("time".into(), [1., 2., 3., 4., 5., 6.]);
+        let s1e = Column::new("FR01".into(), [0., 1., 2., 3., 3., 5.]);
+        let s2e = Column::new("FR02".into(), [0., 0., -2., -2., -1., 1.]);
+        let expected_df = DataFrame::new(vec![s1e, s2e]).expect("Could not initialise dataframe");
+
+        let s1 = Column::new("FR01".into(), [1., 2., 3., 4., 0., 2.]);
+        let s2 = Column::new("FR02".into(), [0., 5., 3., 0., 1., 3.]);
+        let mut df: DataFrame =
+            DataFrame::new(vec![t, s1, s2]).expect("Could not initialise dataframe");
+
+        df = filter_jumps(df, cutoff).expect("Filter failed");
+
+        // assert_eq!(df.shape(), expected_df.shape());
+
+        assert!(df.equals(&expected_df));
+    }
 }
