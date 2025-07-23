25dd7961cc7a0dcbcf43d1a5de806bcf3c00a454
chataignault <leonchat@orange.fr>
2025-07-23 20:33:07 +0100

    add pandas datareader router

--- src/app/main.py
+++ src/app/main.py
@@ -4,11 +4,14 @@ from fastapi.middleware.cors import CORSMiddleware
 from ..utils import *
 from .routers.yf_router import yf_router
 from .routers.ka_router import ka_router
+from .routers.pdr_routers import pdr_router
 
-app = FastAPI(title="IR app backend")
+app = FastAPI(title="Fixed Income app")
 app.include_router(yf_router)
 app.include_router(ka_router)
+app.include_router(pdr_router)
 
+# associated frontend
 origins = [
     "http://localhost:3000/react-tutorial",
 ]
---
+++ src/app/routers/pdr_routers.py
@@ -0,0 +1,29 @@
+import pandas_datareader as pdr
+from fastapi import APIRouter
+
+
+pdr_router = APIRouter(prefix="/pdr", tags=["Pandas Data-Reader proxy"])
+
+
+def get_usd_index_fred():
+    """
+    Get trade-weighted USD index from FRED
+    """
+    try:
+        df = pdr.get_data_fred("DTWEXBGS", start="1985-01-01")
+        df.columns = ["USD_Index"]
+        df.index.name = "date"
+        return df.rename(columns={"USD_Index": "USDX"})
+    except Exception as e:
+        print(f"FRED error: {e}")
+        return None
+
+
+@pdr_router.get("/usdx")
+async def read_usdx(
+    save: bool = False,
+):
+    df = get_usd_index_fred()
+    if save:
+        df.to_parquet("usdx_history.parquet")
+    return df.to_json()
